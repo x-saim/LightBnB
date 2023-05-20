8ac196d41c88a456375364948fb5b55245e9e3ba
Saim Amir <saimxamir@gmail.com>
2023-05-20 02:15:07 -0400

    add insert queries for remaining tables

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -7,3 +7,13 @@ INSERT INTO properties (id, owner_id, title, description, thumbnail_photo_url, c
 VALUES (1, 1001, 'Cozy Cottage', 'description', 'https://example.com/thumbnail1.jpg', 'https://example.com/cover1.jpg', 100, 2, 1, 2, 'United States', '123 Main St', 'New York', 'NY', '12345', true),
 (2, 1002, 'Modern Apartment', 'description', 'https://example.com/thumbnail2.jpg', 'https://example.com/cover2.jpg', 150, 1, 2, 1, 'Canada', '456 Elm St', 'Toronto', 'ON', 'M1A 1A1', true),
 (3, 1003, 'Beach House Retreat', 'description', 'https://example.com/thumbnail3.jpg', 'https://example.com/cover3.jpg', 200, 3, 2, 3, 'Australia', '789 Beach Rd', 'Sydney', 'NSW', '2000', true);
+
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id)
+VALUES (1, '2023-05-21', '2023-05-28', 1, 1001),
+(2, '2023-06-10', '2023-06-15', 2, 1002),
+(3, '2023-07-01', '2023-07-10', 3, 1003);
+
+INSERT INTO property_reviews (id, guest_id, reservation_id, property_id, rating, message)
+VALUES (1, 1001, 1, 1, 5, 'Great property and excellent host!'),
+(2, 1002, 2, 2, 4, 'Enjoyed my stay, but had some minor issues.'),
+(3, 1003, 3, 3, 5, 'Absolutely loved this property. Highly recommended!');
